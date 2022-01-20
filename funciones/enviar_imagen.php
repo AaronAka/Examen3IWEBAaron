daddbcc1dd37ff4e71a77ef94cb7650a6382a6e6
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-01-20 21:05:39 +0100

    Add files via upload

---
+++ funciones/enviar_imagen.php
@@ -0,0 +1,63 @@
+<?php
+$file = $_FILES['imagen'];
+// Si es una imagen continuamos, si no, mandamos el error :3
+$desc = $_POST['descripcion'];
+var_dump($file);
+var_dump($desc);
+$file_size = $file['size'];
+
+if (($file_size > 2*1024*1024)){      
+    echo '<p>File too large. File must be less than 2 MB.</p>'; 
+    
+} else if($file['type'] == 'image/jpg' || $file['type'] == 'image/png' || $file['type'] == 'image/jpeg'){
+    $filename= $file['tmp_name'];
+    $client_id = "531facc897ea14b"; // AQUI SU CLIENT ID
+    $handle = fopen($filename, "r");
+    $data = fread($handle, filesize($filename));
+    $pvars   = array('image' => base64_encode($data));
+    $timeout = 30;
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+    $out = curl_exec($curl);
+    curl_close ($curl);
+    $pms = json_decode($out,true);
+    $url="";
+    
+    if( isset( $pms['data']['link'] ) ){
+        $url=$pms['data']['link'];
+     }
+    
+     $urlEndpoint = 'http://localhost:3000/images/add';
+    
+     $ch = curl_init();
+     curl_setopt($ch, CURLOPT_URL, $urlEndpoint);
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($ch, CURLOPT_POST, true);
+
+     $data = array(
+         "imagen" => $url,
+         "descripcion" => $_POST['descripcion'],
+         "numeroLikes" => 0
+     );
+
+     $json = json_encode($data);
+
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+     $output = curl_exec($ch);
+     $info = curl_getinfo($ch);
+     curl_close($ch); 
+     $result = json_decode($output);
+     
+     $_SESSION['server_msg'] = $result->data->msg;
+     
+}
+
+header ("Location: ../index.php");
+?>
---
+++ includes/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>BlablacarIW</title>
+</head>
+<body>
+        <header>
+            <div class="container">
+                <img src="./resources/logo.svg" height="87" width="100"alt="Imagen logo">
+                
+                <div class="contenido-header">
+                    <h1><a href="../index.php">Home</a></h1>
+                    <?php if (!isset($_SESSION['usuario']->admin)) {?>
+                        <h1><a href="../myaccount.php">Mi Perfil</a></h1>    
+                    <?php }?>
+                    <div class="d-flex justify-content-center py-3">
+                        <ul class="nav nav-pills">
+                            
+                            <li class="nav-item">
+                                <a href="../viaje/crear_viaje.php" class="nav-link">Buscar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../viaje/crear_viaje.php" class="nav-link">Publicar viaje</a>
+                            </li>
+                            <?php 
+                            
+                            if (!isset($_SESSION['login'])){
+                                header ('Location: ../login.php');
+                            }else{?>
+                                <li class="nav-item">
+                                    <a href="logout.php" class="nav-link">Cerrar sesión</a>
+                                </li>
+                            <?php
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
